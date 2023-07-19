cdd27d1fb3cf0dc2558088b8090621097eb53eaf
SalasVirtuales <salas.virtuales@gmail.com>
2023-07-19 07:29:29 -0300

    Clase 07 - Varios

---
+++ borrar/src/borrar/Borrar.java
@@ -0,0 +1,18 @@
+
+package borrar;
+
+
+public class Borrar {
+
+    
+    public static void main(String[] args) {
+        String cadena1 = "Marcelo";
+       String cadena2 = "Marcelo";
+       
+        System.out.println(cadena1.equalsIgnoreCase("Silvia"));
+           
+       
+        
+    }
+    
+}
